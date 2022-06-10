1f32370901e2eb25b02e4456b01687fcce4b2b13
Dossinosaur2k <dinhngoctrang1203@gmail.com>
2022-06-10 10:31:13 +0700

    fix_url_ascii_error

--- websites/ivivu.py
+++ websites/ivivu.py
@@ -39,75 +39,85 @@ def job_ivivu():
     group_links_all = group_link1 + group_link2
     page_links=[site]
     for i in group_links_all:
-        page_links.append(url+i['href'])
+
+        link = (''.join((url,i['href'])).encode('utf-8')).strip()
+        page_links.append(link.decode('utf-8'))
+        
+        
     print(page_links)
     #Lấy thông tin của toàn bộ page trong link
    
     all_df = []
     dfs=[]
     for page in page_links:
-        
-        web_request = Request(page, headers={'User-Agent':'Mozilla/5.0'})
+        try:
+            web_request = Request(page, headers={'User-Agent':'Mozilla/5.0'})
 
-        web_page = urlopen(web_request).read()
-        soup = BeautifulSoup(web_page, 'html.parser')
-        
-        tour_items = soup.find_all('div', attrs={"class":"tourItem"})
-    #     print(tour_items)
-        tour_imgs = []
-        tour_links = []
-        tour_names = []
-        tour_names_no_vietnamese = []
-        tour_prices = []
-        web_logos = []
-        tour_durations = []
-        tour_start_days = []
-        tour_ids = []
-        web_names = []
-        
-        for item in tour_items:
-            tour_price = item.find('span' , attrs={"class":"tourItemPrice"})
-            if (tour_price is not None):
-
-                web_names.append(web_name)
-
-                web_logos.append(url+soup.find('a', attrs={"class":"navbar-brand"}).find('img')['src'])
-                #convert to int 
-#                 tour_price = int(tour_price.get_text().replace('VND','').replace('.','').strip())
-
-                tour_imgs.append(item.find('img')['data-src'])
-    
-                tour_links.append(url+item.find('a' , attrs={"class":"linkDetail"})['href'])
-        
-                tour_names.append(item.find('span' , attrs={"class":"tourItemName"}).get_text().strip())
-
-                tour_names_no_vietnamese.append(unidecode(item.find('span' , attrs={"class":"tourItemName"}).get_text().strip()))
+            web_page = urlopen(web_request).read()
+            soup = BeautifulSoup(web_page, 'html.parser')
             
-                tour_prices.append(int(tour_price.get_text().replace("VND", "").replace('.','').strip()))
+            tour_items = soup.find_all('div', attrs={"class":"tourItem"})
+        #     print(tour_items)
+            tour_imgs = []
+            tour_links = []
+            tour_names = []
+            tour_names_no_vietnamese = []
+            tour_prices = []
+            web_logos = []
+            tour_durations = []
+            tour_start_days = []
+            tour_ids = []
+            web_names = []
+            
+            for item in tour_items:
                 
-                tour_infors = item.find('div', attrs={"class":"v-margin-top-10"}).find_all('span', attrs={"class":"v-margin-right-15"})
+                tour_price = item.find('span' , attrs={"class":"tourItemPrice"})
+                if (tour_price is not None):
 
-                tour_ids.append(tour_infors[0].get_text().replace('Mã: ','').strip())
-                                   
-                tour_durations.append(tour_infors[1].get_text().strip())
-                                   
-                tour_start_days.append(item.find('span',attrs={"class":"tourItemDateTime"}).get_text().strip().replace("Khởi hành:", ""))
-#                 tour = { "TourName": tour_name, "TourLink": tour_link, "TourImg": tour_img, "TourPrice": tour_price }
-#                 insert = mycol.insert_one(tour)
-              
-        df = pd.DataFrame({"Tour_id": tour_ids, 
-                           "Web_name":web_names,
-                           "Web_logo":web_logos, 
-                           "Tour_name": tour_names,
-                           "Tour_name_no_vietnamese":tour_names_no_vietnamese,
-                           "Tour_link": tour_links, 
-                           "Tour_img": tour_imgs,
-                           "Tour_duration":tour_durations,
-                           "Tour_start_day":tour_start_days,
-                           "Tour_price": tour_prices 
-                          })
-                          
-        dfs.append(df)
+                    web_names.append(web_name)
+
+                    web_logos.append(url+soup.find('a', attrs={"class":"navbar-brand"}).find('img')['src'])
+
+                    #convert to int 
+    #                 tour_price = int(tour_price.get_text().replace('VND','').replace('.','').strip())
+
+                    tour_imgs.append(item.find('img')['data-src'])
+        
+                    tour_links.append(url+item.find('a' , attrs={"class":"linkDetail"})['href'])
+                    
+            
+                    tour_names.append(item.find('span' , attrs={"class":"tourItemName"}).get_text().strip())
+
+                    tour_names_no_vietnamese.append(unidecode(item.find('span' , attrs={"class":"tourItemName"}).get_text().strip()))
+                
+                
+                    tour_prices.append(int(tour_price.get_text().replace("VND", "").replace('.','').strip()))
+                    
+                    tour_infors = item.find('div', attrs={"class":"v-margin-top-10"}).find_all('span', attrs={"class":"v-margin-right-15"})
+
+                    tour_ids.append(tour_infors[0].get_text().replace('Mã: ','').strip())
+                                    
+                    tour_durations.append(tour_infors[1].get_text().strip())
+                                    
+                    tour_start_days.append(item.find('span',attrs={"class":"tourItemDateTime"}).get_text().strip().replace("Khởi hành:", ""))
+    #                 tour = { "TourName": tour_name, "TourLink": tour_link, "TourImg": tour_img, "TourPrice": tour_price }
+    #                 insert = mycol.insert_one(tour)
+                
+            df = pd.DataFrame({"Tour_id": tour_ids, 
+                            "Web_name":web_names,
+                            "Web_logo":web_logos, 
+                            "Tour_name": tour_names,
+                            "Tour_name_no_vietnamese":tour_names_no_vietnamese,
+                            "Tour_link": tour_links, 
+                            "Tour_img": tour_imgs,
+                            "Tour_duration":tour_durations,
+                            "Tour_start_day":tour_start_days,
+                            "Tour_price": tour_prices 
+                            })
+                            
+            dfs.append(df)
+        except Exception:
+            print(page)
         # print(df)
     df = pd.concat(dfs, ignore_index=True)
     # df.drop_duplicates(subset=["Tour_id","Tour_name"],keep=False, inplace=True,ignore_index=True)
